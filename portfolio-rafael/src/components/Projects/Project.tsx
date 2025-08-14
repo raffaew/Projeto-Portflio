1ec72244c0194e11a092aa12e3c560764c0efb36
Rafael Moreira Dias <rafael.moreira@afixcode.com.br>
2025-08-13 21:02:01 -0300

    add menu hamburger

--- portfolio-rafael/src/components/Projects/Project.tsx
+++ portfolio-rafael/src/components/Projects/Project.tsx
@@ -30,9 +30,9 @@ const Project = () => {
     <div className="projects">
       <div className="frontEndMentor">
         <div className="description">
-          <h3>Projetos responsivos</h3>
+          <h3>Projetos Responsivos</h3>
           <p>
-            O Frontend Mentor é uma plataforma que oferece desafios práticos
+            O <a href="https://www.frontendmentor.io/" target="blank">Frontend Mentor</a>  é uma plataforma que oferece desafios práticos
             para desenvolvedores front-end com base em designs prontos. Os
             desafios variam de nível iniciante a avançado e são voltados para o
             aprimoramento de habilidades como HTML, CSS, JavaScript,
@@ -52,21 +52,26 @@ const Project = () => {
               <div className="cardContainer" key={project.id}>
                 <div className="card">
                   <div className="images">
-                    <img
-                      src={project.imageUrl}
-                      alt={project.alt}
-                    />
+                    <img src={project.imageUrl} alt={project.alt} />
                   </div>
                 </div>
                 <div className="links">
                   <button>
-                    <a href={project.site} target="_blank" rel="noopener noreferrer">
+                    <a
+                      href={project.site}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
                       Ver Site
                     </a>
                     <FaLink className="icon" />
                   </button>
                   <button>
-                    <a href={project.code} target="_blank" rel="noopener noreferrer">
+                    <a
+                      href={project.code}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
                       Ver Código
                     </a>
                     <FaGithub className="icon" />
@@ -77,6 +82,47 @@ const Project = () => {
           </div>
           <FaChevronRight className="chevronRight" onClick={handleRightClick} />
         </div>
+      </div>
+
+      <div className="tests">
+        <div className="description">
+          <h3>Projetos Com Testes</h3>
+          <p>
+            Nesta seção, apresento um projeto que desenvolvi com foco em testes
+            unitários. A implementação de testes é fundamental para garantir
+            a qualidade e a confiabilidade do código, permitindo que mudanças
+            futuras sejam feitas com mais segurança. 
+          </p>
+
+          <p>
+            Fiz um to-do-list que inclui funcionalidades como adicionar, remover e marcar tarefas como concluídas. Cada funcionalidade foi acompanhada por testes unitários para verificar seu correto funcionamento.
+          </p>
+
+          <p>
+            Os testes foram escritos utilizando a biblioteca Jest, que é amplamente utilizada para testar aplicações JavaScript. Através desses testes, pude garantir que cada parte do código funcionasse como esperado, minimizando bugs e melhorando a manutenção do projeto.
+          </p>
+        </div>
+
+         <div className="cardContainer">
+              <div className="card">
+                <div className="images">
+                  <img
+                    src="/to-do-list.png"
+                    alt="3-column-preview-card"
+                  />
+                </div>
+              </div>
+              <div className="links">
+                <button>
+                  <a href="https://raffaew.github.io/testing-library-com-vite/" target="blank">Ver Site</a>
+                  <FaLink className="icon" />
+                </button>
+                <button>
+                  <a href="https://github.com/raffaew/testing-library-com-vite/tree/main/page-to-do-list" target="blank">Ver Código</a>
+                  <FaGithub className="icon" />
+                </button>
+              </div>
+            </div>
       </div>
     </div>
   );
--- portfolio-rafael/src/components/header/Header.tsx
+++ portfolio-rafael/src/components/header/Header.tsx
@@ -1,8 +1,28 @@
-// Header.jsx
+import { useState, useEffect } from "react";
 import "./header.scss";
 import { Link } from "react-router-dom";
+import { HiMenu } from "react-icons/hi";
+import { IoClose } from "react-icons/io5";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 500) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  console.log("mobile:", isMobile, "menuOpen:", menuOpen, window.innerWidth);
+
   return (
     <header className="header">
       <div className="logo">
@@ -10,7 +30,7 @@ const Header = () => {
       </div>
 
       <nav className="nav-links">
-        <ul>
+        <ul className={`nav-menu ${menuOpen ? "open" : ""}`}>
           <li>
             <Link to="/sobre">Sobre mim</Link>
           </li>
@@ -21,13 +41,25 @@ const Header = () => {
             <Link to="/projetos">Portfólio de Projetos</Link>
           </li>
           <li>
-            <a href="mailto:raffa96dias@gmail.com">Contato</a> 
+            <a href="mailto:raffa96dias@gmail.com">Contato</a>
           </li>
           <li>
-            <a href="/CV_Rafael_Dias.pdf" target="blank">Currículo</a> 
+            <a href="/CV_Rafael_Dias.pdf" target="blank">
+              Currículo
+            </a>
           </li>
         </ul>
       </nav>
+
+      {isMobile && (
+        <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+          {
+            <span className={`icon ${menuOpen ? "open" : ""}`}>
+              {menuOpen ? <IoClose /> : <HiMenu />}
+            </span>
+          }
+        </button>
+      )}
     </header>
   );
 };
